0cf6977c3b2e32b2d0c7a246e34f1218a1b99f01
Ashwin Sethu Baskaran <ashwinbaskaran94@gmail.com>
2018-05-24 00:59:55 -0400

    Setting up Git

--- BlockChain/Program.cs
+++ BlockChain/Program.cs
@@ -6,7 +6,7 @@ namespace BlockChain
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Blockchain");
         }
     }
 }
