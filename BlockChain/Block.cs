73ca7cae0a9bb1393ec5607153a6806a736202a1
Ashwin SethuBaskaran <ashwinsethubaskaran@amrock.com>
2018-07-16 16:39:09 -0400

    Defining the block

---
+++ BlockChain/Block.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlockChain
+{
+    class Block
+    {
+        public string hash;
+        public string previousHash;
+        public string data;
+        private string timeStamp;
+
+        public Block(string data, string previousHash)
+        {
+            this.data = data;
+            this.previousHash = previousHash;
+            timeStamp = DateTimeOffset.Now.ToUnixTimeMilliseconds().ToString();
+        }
+    }
+}
