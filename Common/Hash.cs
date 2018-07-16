04c42b41aa5e8066d8876c4a9e0f2106b929516c
Ashwin SethuBaskaran <ashwinsethubaskaran@amrock.com>
2018-07-16 17:46:57 -0400

    Added SHA 256 hashing

---
+++ Common/Hash.cs
@@ -0,0 +1,20 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace Common
+{
+    public class Hash
+    {
+        public static string ApplySha256(string input)
+        {
+            var crypt = new SHA256Managed();
+            var hash = new StringBuilder();
+            byte[] crypto = crypt.ComputeHash(Encoding.UTF8.GetBytes(input));
+            foreach (byte theByte in crypto)
+            {
+                hash.Append(theByte.ToString("x2"));
+            }
+            return hash.ToString();
+        }
+    }
+}
